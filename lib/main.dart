91efc4b30fc5a402a40905564085567e81be82e7
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-06-11 02:26:22 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:loginplant/editarPerfil.dart'; //cambia la ruta con el nombre de tu aplicacion, en mi caso se llama 'prueba' , sino no va a funcionar
-
+import 'package:flutter/services.dart';
+import 'package:the_gorgeous_login/pages/login_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Editar Perfil ',
-        theme: ThemeData(
-            primaryColor: Colors.blue
-        ),
-        home: editarPerfil(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TheGorgeousLogin',
+      home: LoginPage(),
     );
   }
 }
